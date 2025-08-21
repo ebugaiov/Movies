8236e9097e9b9dd734a8c52dbaf3d659cb5ed839
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-08-21 11:00:04 +0200

    Creating a temporary database

---
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using Movies.Application.Models;
+
+namespace Movies.Application.Repositories;
+
+public interface IMovieRepository
+{
+    Task<bool> CreateAsync(Movie movie);
+    
+    Task<bool> UpdateAsync(Movie movie);
+    
+    Task<bool> DeleteByIdAsync(Guid id);
+    
+    Task<Movie?> GetByIdAsync(Guid id);
+    
+    Task<IEnumerable<Movie>> GetAllAsync();
+}

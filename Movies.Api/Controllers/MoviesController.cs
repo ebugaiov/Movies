a418a95d0aadc8db6fdea11391e5b8330f0705da
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-08-21 11:38:35 +0200

    Create simple mapper for create movie

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Movies.Application.Models;
+using Movies.Api.Mapping;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
 
@@ -19,13 +19,7 @@ namespace Movies.Api.Controllers
         [HttpPost("movies")]
         public async Task<IActionResult> CreateMovie(CreateMovieRequest request)
         {
-            var movie = new Movie
-            {
-                Id = Guid.NewGuid(),
-                Title = request.Title,
-                YearOfRelease = request.YearOfRelease,
-                Genres = request.Genres.ToList(),
-            };
+            var movie = request.MapToMovie();
             await _movieRepository.CreateAsync(movie);
             return Created($"/api/movies/{movie.Id}", movie);
         }

c74378762aad920645124d8c1a018afa4a8da70d
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-08-21 13:17:48 +0200

    Implement movie retrival

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -22,5 +22,22 @@ namespace Movies.Api.Controllers
             await _movieRepository.CreateAsync(movie);
             return Created($"{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
         }
+
+        [HttpGet(ApiEndpoints.Movies.Get)]
+        public async Task<IActionResult> GetMovie(Guid id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie is null) return NotFound();
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+
+        [HttpGet(ApiEndpoints.Movies.GetAll)]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+            var response = movies.MapToResponse();
+            return Ok(response);
+        }
     }
 }

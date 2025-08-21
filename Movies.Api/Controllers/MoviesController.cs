50bb4bd06c4ed3bb30a80811f5d2a42280450e32
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-08-21 11:14:28 +0200

    Create movies controller

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+        
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+    }
+}

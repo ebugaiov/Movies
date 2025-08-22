37a8e599cfb21dfe2b5e3d89e4b90cb01a0c1686
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-08-22 15:20:56 +0200

    Add business logic layer

---
+++ Movies.Application/Services/IMovieService.cs
@@ -0,0 +1,18 @@
+using Movies.Application.Models;
+
+namespace Movies.Application.Services;
+
+public interface IMovieService
+{
+    Task<bool> CreateAsync(Movie movie);
+    
+    Task<Movie?> UpdateAsync(Movie movie);
+    
+    Task<bool> DeleteByIdAsync(Guid id);
+    
+    Task<Movie?> GetByIdAsync(Guid id);
+    
+    Task<Movie?> GetBySlugAsync(string slug);
+    
+    Task<IEnumerable<Movie>> GetAllAsync();
+}

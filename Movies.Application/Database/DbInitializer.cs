706a5139f0944d9021ac59055a86b687e7411160
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-08-25 14:54:46 +0200

    Init ratings

--- Movies.Application/Database/DbInitializer.cs
+++ Movies.Application/Database/DbInitializer.cs
@@ -29,7 +29,18 @@ public class DbInitializer
         await connection.ExecuteAsync("""
             create table if not exists genres (
                 movieId UUID references movies(Id),
-                name text not null);
+                name text not null
+            );
+        """);
+        
+        // Create ratings table
+        await connection.ExecuteAsync("""
+            create table if not exists ratings (
+                userid uuid,
+                movieid uuid references movies(id),
+                rating integer not null,
+                primary key (userid, movieid)
+            );
         """);
         
         // Create unique index on slug

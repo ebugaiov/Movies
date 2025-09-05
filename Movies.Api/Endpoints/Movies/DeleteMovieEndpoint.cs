5ebcfeb067ee65c7d2bbf3ec686aa3f556ca852f
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-09-05 14:52:52 +0200

    Migrating movie deletion on Minimal Api

---
+++ Movies.Api/Endpoints/Movies/DeleteMovieEndpoint.cs
@@ -0,0 +1,24 @@
+using Movies.Application.Services;
+
+namespace Movies.Api.Endpoints.Movies;
+
+public static class DeleteMovieEndpoint
+{
+    public const string Name = "DeleteMovie";
+
+    public static IEndpointRouteBuilder MapDeleteMovie(this IEndpointRouteBuilder app)
+    {
+        app.MapDelete(ApiEndpoints.Movies.Delete, async (
+            Guid id,
+            IMovieService movieService,
+            CancellationToken token) =>
+        {
+            var deleted = await movieService.DeleteByIdAsync(id, token);
+            if (!deleted) return Results.NotFound();
+            return Results.NoContent();
+        })
+        .WithName(Name);
+        
+        return app;
+    }
+}
--- Movies.Api/Endpoints/Movies/MovieEndpointExtensions.cs
+++ Movies.Api/Endpoints/Movies/MovieEndpointExtensions.cs
@@ -8,7 +8,7 @@ public static class MovieEndpointExtensions
         app.MapCreateMovie();
         app.MapGetAllMovies();
         app.MapUpdateMovie();
-        // app.MapDeleteMovie();
+        app.MapDeleteMovie();
         return app;
     }
 }
